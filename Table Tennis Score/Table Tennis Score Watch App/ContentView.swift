0cd507385d9af4142f4f5e5affec33fa10cd8161
Toni Sucic <toni.sucic@icloud.com>
2023-02-18 12:39:14 +0100

    Add 41mm and 45mm Apple Watch preview devices

--- Table Tennis Score/Table Tennis Score Watch App/ContentView.swift
+++ Table Tennis Score/Table Tennis Score Watch App/ContentView.swift
@@ -210,6 +210,14 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "Apple Watch Series 7 - 41mm"))
+                .previewDisplayName("41 mm")
+
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "Apple Watch Series 7 - 45mm"))
+                .previewDisplayName("45 mm")
+        }
     }
 }
